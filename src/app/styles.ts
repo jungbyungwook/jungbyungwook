173bffb4dbad37a378ad392aa43017907f344ac9
jungbyungwook <korjby400@gmail.com>
2024-06-19 17:38:17 +0900

    feat: 모바일에서 padding

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -5,6 +5,11 @@ const HomeLayout = styled.main`
     flex-direction: column;
     align-items: center;
     margin-top: 100px;
+
+    @media (max-width: 1023px) {
+        padding-left: 16px;
+        padding-right: 16px;
+    }
 `;
 
 const ContentsBox = styled.div``;
